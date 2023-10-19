f88536278bdf0e176745dea695234a3d4285627b
ww-samuel-hale <samuel.hale@woodwing.com>
2023-10-19 12:09:17 -0400

    Reorganized c.py for readability

--- c.py
+++ c.py
@@ -25,6 +25,54 @@ MAX_LINE = 256 # Maximum number of bytes to receive
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a TCP socket
 s.connect((SERVER_HOST, SERVER_PORT)) # Connect to server address
 
+def main():
+    while not QUIT:
+        user = []
+        while user_input != "1" and user_input != "2" and user_input != "3" and user_input != "4" and user_input != "5" and user_input != "6":
+            user_input = menu()
+            if (user_input != "1" and user_input != "2" and user_input != "3" and user_input != "4" and user_input != "5" and user_input != "6" and user_input != "7" and user_input != "8" and user_input != "9" and user_input != "10"):
+                print("c: Invalid input. Please enter a number between 1 and 6.")
+            if user_input == "1" and user != []:
+                buy_route(user)
+            if user_input == "2" and user != []:
+                sell_route(user)
+            if user_input == "3" and user != []:
+                list_route(user)
+            if user_input == "4" and user != []:
+                balance_route(user)
+            if user_input == "5" and user != [] and user[3] == "Root":
+                print("c: SERVER SHUT DOWN")
+                #   - Send message to server to shut down
+                s.sendall(b"SHUTDOWN\n")
+                data = s.recv(MAX_LINE)
+                print("c:", data.decode().strip())
+            if user_input == "6" and user != [] and user[3] == "Root":
+                print("c: CLIENT SHUT DOWN")
+                #  - Check if server is running
+                if check_server_status():
+                    try:
+                        #   - Send QUIT message to server
+                        s.sendall("QUIT".encode())
+                        #   - Wait for confirmation message from server
+                        data = s.recv(MAX_LINE)
+                        if b"200 OK" in data:
+                            QUIT = True
+                        s.close()
+                    except ConnectionAbortedError:
+                        pass
+                QUIT = True
+            if user_input == "7":
+                user = login_route()
+            if user_input == "8" and user != []:
+                user = []
+                print("c: LOGOUT")
+                #  - Code logout functionality in a separate module
+            if user_input == "9" and user != []:
+                print("c: WHO")
+            if user_input == "10" and user != []:
+                print("c: LOOKUP")
+            user_input = ""
+            
 def buy_route(user):
     global s
     from cmodules.Buy import Buy
@@ -79,51 +127,5 @@ def menu():
     
     # Return user input
     return option  
-
-if __name__ == "__main__":
-    while not QUIT:
-        user = []
-        while user_input != "1" and user_input != "2" and user_input != "3" and user_input != "4" and user_input != "5" and user_input != "6":
-            user_input = menu()
-            if (user_input != "1" and user_input != "2" and user_input != "3" and user_input != "4" and user_input != "5" and user_input != "6" and user_input != "7" and user_input != "8" and user_input != "9" and user_input != "10"):
-                print("c: Invalid input. Please enter a number between 1 and 6.")
-            if user_input == "1" and user != []:
-                buy_route(user)
-            if user_input == "2" and user != []:
-                sell_route(user)
-            if user_input == "3" and user != []:
-                list_route(user)
-            if user_input == "4" and user != []:
-                balance_route(user)
-            if user_input == "5" and user != [] and user[3] == "Root":
-                print("c: SERVER SHUT DOWN")
-                #   - Send message to server to shut down
-                s.sendall(b"SHUTDOWN\n")
-                data = s.recv(MAX_LINE)
-                print("c:", data.decode().strip())
-            if user_input == "6" and user != [] and user[3] == "Root":
-                print("c: CLIENT SHUT DOWN")
-                #  - Check if server is running
-                if check_server_status():
-                    try:
-                        #   - Send QUIT message to server
-                        s.sendall("QUIT".encode())
-                        #   - Wait for confirmation message from server
-                        data = s.recv(MAX_LINE)
-                        if b"200 OK" in data:
-                            QUIT = True
-                        s.close()
-                    except ConnectionAbortedError:
-                        pass
-                QUIT = True
-            if user_input == "7":
-                user = login_route()
-            if user_input == "8" and user != []:
-                user = []
-                print("c: LOGOUT")
-                #  - Code logout functionality in a separate module
-            if user_input == "9" and user != []:
-                print("c: WHO")
-            if user_input == "10" and user != []:
-                print("c: LOOKUP")
-            user_input = ""
+            
+main()
