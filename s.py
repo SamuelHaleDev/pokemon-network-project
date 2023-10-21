1f052db2d4811ce8de146f83f453b08d3d207fe8
ww-samuel-hale <samuel.hale@woodwing.com>
2023-10-21 17:25:16 -0400

    Finished initial implementation of multi threading

--- s.py
+++ s.py
@@ -2,16 +2,14 @@ import socket
 import sqlite3
 from concurrent.futures import ThreadPoolExecutor
 import threading
-
-con = sqlite3.connect("Pokemon.db") # Connect to database
-
-cur = con.cursor() # Create cursor object
+import queue
 
 # Define server port
 PORT = 4987 # Port number is a 16-bit unsigned integer
 MAX_PENDING = 5 # Maximum number of pending connections
 MAX_LINE = 256 # Maximum number of bytes to receive
 connected_clients = 0
+connection_pool = queue.Queue(maxsize=MAX_PENDING)
 
 clients_lock = threading.Lock()
 
@@ -26,9 +24,21 @@ def main():
             conn, addr = s.accept() # Accept a connection
             
             executor.submit(handle_client_route, conn, addr)
+            
+def create_connection():
+    return sqlite3.connect("Pokemon.db")
+
+def get_connection():
+    global MAX_PENDING, connection_pool
+    with clients_lock:
+        if connection_pool.qsize() < MAX_PENDING:
+            connection_pool.put(create_connection())
+        return connection_pool.get()
 
 def handle_client_route(conn, addr):
-    global connected_clients, MAX_LINE, s, con, cur
+    global connected_clients, MAX_LINE, s
+    con = get_connection()
+    cur = con.cursor()
     with clients_lock:
         connected_clients += 1
         print(f"s: New connection from {addr}. {connected_clients} connected clients.")
