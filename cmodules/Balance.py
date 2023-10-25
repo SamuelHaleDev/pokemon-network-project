79b7f28c122c5106977eb98b130033d9c6652487
ww-samuel-hale <samuel.hale@woodwing.com>
2023-10-25 11:06:30 -0400

    Added error handling for Balance

--- cmodules/Balance.py
+++ cmodules/Balance.py
@@ -1,15 +1,20 @@
 def Balance(user, s, MAX_LINE, request_queue, response_queue):
-    #User Checks their balance
-    userID = user[0]
-    money = "BALANCE " + userID
-    request_queue.put(money)
-    money = str(response_queue.get())
-    money = money.split("'")[1]
-    balance = ""
-    for c in money: 
-            if c.isdigit() or (c == '.'): 
-                balance = balance + c
+    try:
+        #User Checks their balance
+        userID = user[0]
+        money = "BALANCE " + userID
+        request_queue.put(money)
+        money = str(response_queue.get())
+        money = money.split("'")[1]
+        balance = ""
+        for c in money: 
+                if c.isdigit() or (c == '.'): 
+                    balance = balance + c
 
-    print(balance)
+        print(balance)
+    except Exception as e:
+        print(f"c: An error occurred: {e}")
+        print("c: Balance failed.")
+        return
     
 __all__ = ["Balance"]
