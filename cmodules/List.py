95d46fad5b0e6ee0af45d2c35dbec21e8ca6d39f
ww-samuel-hale <samuel.hale@woodwing.com>
2023-10-25 08:58:53 -0400

    Implemented threading for List in Client

--- cmodules/List.py
+++ cmodules/List.py
@@ -1,3 +1,5 @@
+import ast
+
 def List(user, s, MAX_LINE, request_queue, response_queue):
     #  - BUILD CLIENT REQUEST
     print("c: LISTING ALL RECORDS IN POKEMON CARDS TABLE")
@@ -7,9 +9,8 @@ def List(user, s, MAX_LINE, request_queue, response_queue):
     
     #  - SEND AND RECEIVE DATA
     request_queue.put(client_request)
-    data = s.recv(MAX_LINE)
-    if b"200 OK" in data:
-        response = data.decode().split("|")[1].strip()
-        print(response)
+    data = response_queue.get()
+    response = ast.literal_eval(data)
+    print(response)
         
 __all__ = ["List"]
