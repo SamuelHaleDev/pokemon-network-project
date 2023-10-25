fd44da46f8562d64669b3323c787e0b12451f66c
ww-samuel-hale <samuel.hale@woodwing.com>
2023-10-25 11:05:53 -0400

    Added error handling for logout

--- cmodules/Logout.py
+++ cmodules/Logout.py
@@ -1,14 +1,19 @@
 def Logout(user, s, MAX_LINE, request_queue, response_queue):
-    user_name = user[3]
-    data = f"LOGOUT {user_name}\n"
-    
-    request_queue.put(data)
-    data = response_queue.get()
-    
-    if "200" in data:
-        print("c: Logout successful.")
-        return []
-    else:
+    try:
+        user_name = user[3]
+        data = f"LOGOUT {user_name}\n"
+        
+        request_queue.put(data)
+        data = response_queue.get()
+        
+        if "200" in data:
+            print("c: Logout successful.")
+            return []
+        else:
+            print("c: Logout failed.")
+            return user
+    except Exception as e:
+        print(f"c: An error occurred: {e}")
         print("c: Logout failed.")
         return user
     
